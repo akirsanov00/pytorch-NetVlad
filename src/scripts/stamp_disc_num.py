015c8e6e5d45fd61b79254d34823168779c7436a
Aleksandr Kirsanov <akirsanov00@gmail.com>
2025-07-13 14:15:40 +0300

    stamp_disc_num.py

--- src/scripts/stamp_disc_num.py
+++ src/scripts/stamp_disc_num.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import splprep, splev
 from glob import glob
 
-images_folder = '../../images'
+images_folder = '../../data/images'
 output_json = '../output/raw_dataset.json'
 
 
